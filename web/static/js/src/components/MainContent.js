f1901369647d10bacfc424eea90a7fd0acfc153f
terakilobyte <terakilobyte@gmail.com>
2016-01-02 10:43:11 -0800

    update styling for logo, removes jumbotron

--- web/static/js/src/components/MainContent.js
+++ web/static/js/src/components/MainContent.js
@@ -1,6 +1,6 @@
 const MainContent = () => {
   return (
-    <div className='jumbotron'>
+    <div className='container-fluid text-center'>
       <h2>Welcome to Vot.io, a realtime voting service!</h2>
     </div>
   );
