d83e864fc7faa0862be1e9b88d1f2d951da31be7
terakilobyte <terakilobyte@gmail.com>
2016-01-02 10:41:42 -0800

    removes styling testing

--- web/static/js/tests/components/MainContent.spec.js
+++ web/static/js/tests/components/MainContent.spec.js
@@ -20,8 +20,4 @@ describe('(Component) MainContent', function () {
   it('Should render as a <div>.', function () {
     expect(_component.type).to.equal('div');
   });
-
-  it('Should have class \'jumbotron\'', () => {
-    expect(_component.props.className).to.includes('jumbotron');
-  });
 });
