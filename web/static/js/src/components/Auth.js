e5b3c08f77f5a86ca5bb6d521d6ed9f02fb3d7cb
terakilobyte <terakilobyte@gmail.com>
2016-01-06 20:02:26 -0800

    new less cpu intensive spinner and auth component

---
+++ web/static/js/src/components/Auth.js
@@ -0,0 +1,30 @@
+import {connect} from 'react-redux';
+import {jwtRequest} from 'actions/auth';
+import Spinner from 'components/Spinner';
+
+const mapStateToProps = () => ({});
+
+const mapDispatchToProps = (dispatch) => ({dispatch});
+
+class Auth extends React.Component {
+
+  static propTypes = {
+    history: React.PropTypes.object.isRequired,
+    dispatch: React.PropTypes.func.isRequired
+  }
+
+  constructor (props) {
+    super(props);
+  }
+
+  componentDidMount () {
+    jwtRequest(this.props.dispatch, this.props.history);
+  }
+
+  render () {
+    return (
+      <Spinner />
+    );
+  }
+}
+export default connect(mapStateToProps, mapDispatchToProps)(Auth);
